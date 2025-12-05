cdbf19c1d19fb80034ec94faa3699021136e57f7
Nurken <nurken4ik2010@gmail.com>
2025-12-06 00:21:53 +0500

    Fix the front

--- qazfinance_site/settings.py
+++ qazfinance_site/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'core' / 'static',
+]
 
 # WhiteNoise configuration for static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
